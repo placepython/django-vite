c933ea3129bd2c5135a1f3504f188a62eb6a85d5
Thierry Chappuis <tchappui@users.noreply.github.com>
2024-05-03 13:07:42 +0200

    Update pre_gen_project.py

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,6 +1,6 @@
 import sys
 
-if {{cookiecutter.use_tailwindcss}} and {{cookiecutter.use_bootstrap}}:
+if {{{ cookiecutter.use_tailwindcss }}} and {{{ cookiecutter.use_bootstrap }}}:
     print(
         "You should either use TailwindCSS or Bootstrap 5, "
         "but not both of them !"
