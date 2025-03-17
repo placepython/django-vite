279a1910891378dc3daaa0429a0aaf9f1ba6bbdb
Jonathan Piaget <piagetjonathan@protonmail.com>
2025-03-17 19:17:31 +0100

    Update dependencies and configuration for Tailwind CSS 4 and DaisyUI 5 integration

--- {{{ cookiecutter._frontend_dirname }}}/postcss.config.mjs
+++ {{{ cookiecutter._frontend_dirname }}}/postcss.config.mjs
@@ -1,17 +1,5 @@
-{{%- if cookiecutter.use_tailwindcss -%}}
-export default {
-  plugins: {
-    "postcss-import": {},
-    "tailwindcss/nesting": {},
-    tailwindcss: {},
-    "postcss-preset-env": {
-      features: { "nesting-rules": false },
-    },
-  },
-};
-{{%- else -%}}
 export default {
   plugins: {
     autoprefixer: {}
   },
-};{{% endif %}}
+};
--- {{{ cookiecutter._frontend_dirname }}}/tailwind.config.js
+++ {{{ cookiecutter._frontend_dirname }}}/tailwind.config.js
@@ -9,8 +9,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [{{% if cookiecutter.use_daisyui %}}
-    require('daisyui'),
-  {{% endif %}}],
 }
 
