1c2162d44e6161243da0af0cd447c8f3d8f84866
Thierry Chappuis <tchappui@users.noreply.github.com>
2025-04-18 18:13:44 +0200

    Removed vite-plugin-stimulus-hmr from vite.config.mjs

--- {{{ cookiecutter._frontend_dirname }}}/vite.config.mjs
+++ {{{ cookiecutter._frontend_dirname }}}/vite.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "vite";
-{{%- if cookiecutter.use_stimulus %}}
-import StimulusHMR from 'vite-plugin-stimulus-hmr';{{% endif %}}
 {{%- if cookiecutter.use_tailwindcss %}}
 import tailwindcss from "@tailwindcss/vite";{{% endif %}}
 
 // Configuration entry point
 export default defineConfig({
   plugins: [
-    {{%- if cookiecutter.use_stimulus %}}
-    StimulusHMR(),{{% endif %}}
     {{%- if cookiecutter.use_tailwindcss %}}
     tailwindcss(),
   {{% endif %}}],
