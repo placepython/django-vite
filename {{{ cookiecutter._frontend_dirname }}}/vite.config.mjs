0a9192712b59301da3299bae7591ee19e24c5ecf
Thierry Chappuis <tchappui@users.noreply.github.com>
2025-10-15 06:52:22 +0200

    Update vite.config.mjs

--- {{{ cookiecutter._frontend_dirname }}}/vite.config.mjs
+++ {{{ cookiecutter._frontend_dirname }}}/vite.config.mjs
@@ -1,21 +1,42 @@
 import { defineConfig } from "vite";
+import path from "path";
 {{%- if cookiecutter.use_tailwindcss %}}
 import tailwindcss from "@tailwindcss/vite";{{% endif %}}
 
-// Configuration entry point
-export default defineConfig({
-  plugins: [
-    {{%- if cookiecutter.use_tailwindcss %}}
-    tailwindcss(),
-  {{% endif %}}],
-  base: "/static/",
-  build: {
-    manifest: "manifest.json",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        app: "./src/main.js"
-      }
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+  const devHost = "localhost";
+  const devPort = 5173;
+  const devOrigin = isDev ? `http://${devHost}:${devPort}` : "";
+
+  return {
+    base: devOrigin + "/static/",
+
+    plugins: [
+      {{%- if cookiecutter.use_tailwindcss %}}
+      tailwindcss(),
+    {{% endif %}}],
+
+    server: {
+      host: devHost,
+      port: devPort,
+      strictPort: true,
+      origin: devOrigin,
     },
-  },
+
+    build: {
+      manifest: true,
+      outDir: "./dist/",
+      emptyOutDir: true,
+      rollupOptions: {
+        main: "./src/main.js",
+      },
+    },
+
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
 });
